e7d21424de804e56eba8c4f5c849def1970ca237
Diego Pineda <pineda.diego798@gmail.com>
2020-09-21 10:25:19 -0500

    Updates

---
+++ precache-manifest.3129563e1262d0371a4c8b33adc65083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d13c1d78a63f4eaa92759482e1f64d",
+    "url": "/react-basic/index.html"
+  },
+  {
+    "revision": "e1709b1699294f39dbb6",
+    "url": "/react-basic/static/css/main.2260781b.chunk.css"
+  },
+  {
+    "revision": "8fa44e667daca78ddd86",
+    "url": "/react-basic/static/js/2.d287af7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-basic/static/js/2.d287af7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1709b1699294f39dbb6",
+    "url": "/react-basic/static/js/main.ab5d998d.chunk.js"
+  },
+  {
+    "revision": "1245b250d3d089543d3d",
+    "url": "/react-basic/static/js/runtime-main.58c57ee8.js"
+  }
+]);
